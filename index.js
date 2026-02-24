2063534cf470ecc7cfdd578fce34b9aad9d96afc
rialfi49 <alfiyyayusri@gmail.com>
2026-02-24 14:20:18 +0700

    commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from "express";
 
 const app = express();
+
+// app.use((req, res, next) => {
+//   console.log(`lewat jalur ini${req.path}`);
+//   next();
+// });
+app.use((err, req, res, next) => {
+  if (false) {
+    next(new Error("Not Authorized"));
+    return;
+  }
+  next();
+});
+
+app.use((err, req, res, next) => {
+  res.send("Error Occured");
+});
+
 app.get("/", (req, res) => {
   res.send("Halo Yusri Alfiyya!");
 });
@@ -10,6 +27,10 @@ app.get("/say/:greeting", (req, res) => {
   res.send(greeting);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+app.listen(3000);
+// app.listen(3000, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
+
+// app.use((err, req, res, next) => {
+//   res.send('Error Occured')};)
