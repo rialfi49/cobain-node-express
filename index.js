e88367458ec4bb48a59feb7e359e7cc6028f846a
rialfi49 <alfiyyayusri@gmail.com>
2026-02-26 17:15:45 +0700

    rrrr

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import notesRouter from "./routes/notes.js";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
+
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("YUSRI DISINI");
-});
-
 app.use("/notes", notesRouter);
 
-mongoose.connect(process.env.MONGODB_URI);
+await mongoose.connect(process.env.MONGO_URI);
+console.log("Database connected");
 
-export default app;
+app.listen(8080, () => {
+  console.log("Server running on port 3000");
+});
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import notesRouter from "./routes/notes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("YUSRI DISINI");
+// });
+
+// app.use("/notes", notesRouter);
+
+// const PORT = process.env.PORT || 3000;
+
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => {
+//     console.log("Database connected");
+
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Database connection error:", err);
+//   });
